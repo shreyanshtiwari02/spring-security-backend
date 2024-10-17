4b1ab374bfb188b980463bc9664bd5e99a8e27bc
Solivagant14 <nishant.jha.360@gmail.com>
2024-10-17 15:05:22 +0530

    lessgooooo

--- src/main/java/com/shrey/react_springboot/controller/HelloController.java
+++ src/main/java/com/shrey/react_springboot/controller/HelloController.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
--- src/main/java/com/shrey/react_springboot/controller/StudentController.java
+++ src/main/java/com/shrey/react_springboot/controller/StudentController.java
@@ -1,18 +1,12 @@
 package com.shrey.react_springboot.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.shrey.react_springboot.entity.Student;
+import com.shrey.react_springboot.model.Student;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
